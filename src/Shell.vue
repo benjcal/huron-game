04502ec6bb5b920b41943bec1f4f64d87ca1aca7
Benjamin Calderon <benj.calderon@gmail.com>
2020-11-25 06:45:07 -0800

    progress

---
+++ src/Shell.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex justify-center w-full h-full bg-brand">
+    <div class="container">
+      <div class="text-white text-8xl">Huron Game</div>
+
+      <div class="flex">
+        <!-- selector of sets available -->
+        <div
+          class="px-4 py-2 mr-1 bg-white border-2 border-gray-400 border-opacity-50 rounded-md w-96"
+        >
+          <div>Ending S Set</div>
+          <div>Transportation Set</div>
+          <div>Professions Set</div>
+        </div>
+
+        <!-- description -->
+        <div
+          class="px-4 py-2 bg-white border-2 border-gray-400 border-opacity-50 rounded-md"
+        >
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo tempora
+            ipsum dicta quae dolor. Sint assumenda nesciunt ab molestiae error
+            non, fugiat mollitia eum eius dolores ullam rerum tenetur maxime!
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+}
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Shell from './Shell.vue'
 import './styles/main.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(Shell),
+}).$mount('#shell')
